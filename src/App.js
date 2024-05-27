bbee14a60e032d11123171876f13a3604e2cc9bd
sandrbuz <sandrbuz@mail.ru>
2024-05-27 22:05:48 +0300

    Finished (in addition I made an intermediate adaptive)

--- src/App.js
+++ src/App.js
@@ -2,30 +2,24 @@ import './App.css';
 import { Card } from './Card';
 
 const data = [
-  { id: 1, title: 'Бюджет', number: 94, description: 'Проверка настроек лимитов бюджета, работы со ставками и динамикой расходов', borderColor: '#E14F4D' },
-  { id: 2, title: 'Структура аккаунта', number: 234, description: 'Правильность распределения рекламных кампаний по структуре аккаунта', borderColor: '#E14F4D' },
-  { id: 3, title: 'Настройки кампании', number: 12, description: 'Корректность настроек рекламных кампаний', borderColor: '#799F18' },
-  { id: 4, title: 'Качество объявлений', number: 76, description: 'Тексты, ссылки, доступность посадочных страниц и модерация', borderColor: '#E8AF4E' },
-  { id: 5, title: 'Таргетинг и семантическое ядро', number: 321, description: 'Наличие минус-слов, показы по запросам и черного списка, а также скидки в динамике охвата', borderColor: '#E14F4D' }
+  { id: 1, title: 'Бюджет', number: 94, description: 'Проверка настройки лимитов бюджета, работу со ставками и динамика расходов', borderColor: '#E14F4D',gridArea: 'a' },
+  { id: 2, title: 'Структура аккаунта', number: 234, description: 'Правильность распределения рекламных кампаний по структуре аккаунта', borderColor: '#E14F4D',gridArea: 'b' },
+  { id: 3, title: 'Настройки кампании', number: 12, description: 'Корректность настроек рекламных кампаний', borderColor: '#799F18',gridArea: 'c' },
+  { id: 4, title: 'Качество объявлений', number: 76, description: 'Тексты, ссылки, доступность посадочных страниц и модерация', borderColor: '#E8AF4E',gridArea: 'd' },
+  { id: 5, title: 'Таргетинг и семантическое ядро', number: 321, description: 'Наличие минус-слов, показы по запросам и черного списка, а также скачки в динамике охвата', borderColor: '#E14F4D',gridArea: 'e' }
 ];
-
 
 function App() {
   return (
     <div className="App">
-
       <div className='main'>
-
         <h1>Замечания и рекомендации</h1>
-
         <div className="recommendations">
           {data.map(item => (
-            <Card key={item.id} borderColor={item.borderColor} title={item.title} number={item.number} description={item.description}/>
+            <Card key={item.id} borderColor={item.borderColor} title={item.title} number={item.number} description={item.description} gridArea={item.gridArea}/>
           ))}
         </div>
-
       </div>
-
     </div>
   );
 }
--- src/Card.jsx
+++ src/Card.jsx
@@ -3,9 +3,11 @@ import s from "./Card.module.css"
 
 export const Card = (props) => {
     return (
-        <div className={s.recommendationItem} style={{ borderLeft: `4px solid ${props.borderColor}` }}>
-            <h3>{props.title}</h3>
-            <p className={s.number}>{props.number}</p>
+        <div className={s.recommendationItem} style={{ borderLeft: `4px solid ${props.borderColor}`, gridArea: props.gridArea }}>
+            <div className={s.topCard}>
+                <h3>{props.title}</h3>
+                <p className={s.number}>{props.number}</p>
+            </div>
             <p className={s.description}>{props.description}</p>
         </div>
     )
